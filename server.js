eadde6561d9a014e9516faea8a6d20bb099fe700
syifacts <ss4166269@gmail.com>
2024-12-08 14:13:43 +0700

    first commit

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const agrowisataRoutes = require('./routes/routes');
 const startMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (err) {
