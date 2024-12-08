939e67b8572fb2f1465bc547eb0ee0add95f2ae8
syifacts <ss4166269@gmail.com>
2024-12-08 15:11:21 +0700

    bismillah

--- server.js
+++ server.js
@@ -20,14 +20,24 @@ const init = async () => {
         host: '0.0.0.0',
     });
 
-    // Register CORS middleware
-    server.ext('onPreResponse', (request, h) => {
-        const response = h.continue;
+// Register CORS middleware
+server.ext('onPreResponse', (request, h) => {
+    const response = request.response;
+
+    if (response.isBoom) {
+        // If the response is an error, we can still set CORS headers
+        response.output.headers['Access-Control-Allow-Origin'] = '*';
+        response.output.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
+        response.output.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    } else {
+        // For successful responses
         response.headers['Access-Control-Allow-Origin'] = '*';
         response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'; // Tambahkan header yang diperlukan
-        return response;
-    });
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    }
+
+    return h.continue;
+});
 
     // Register routes
     server.route(agrowisataRoutes);
