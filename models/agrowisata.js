791876a616d77ca493118b164b8e10de525e0b94
syifacts <ss4166269@gmail.com>
2024-12-09 19:12:54 +0700

    first commit

--- models/agrowisata.js
+++ models/agrowisata.js
@@ -1,14 +1,14 @@
-const { nanoid } = require('nanoid');
 const mongoose = require('mongoose');
+const { nanoid } = require('nanoid');
 
 const agrowisataSchema = new mongoose.Schema({
   id: { type: String, default: () => nanoid(), unique: true },
   name: { type: String, required: true },
   location: { type: String, required: true },
-  urlimg: { type: String, default: '' },
+  urlimg: { type: String, required: true }, // Pastikan urlimg wajib ada
   urlmaps: { type: String, required: true },
   fasilitas: { type: String, required: true },
-}, { _id: false }); // Menonaktifkan pembuatan _id
+}, { _id: false }); // Nonaktifkan pembuatan _id
 
 
 const Agrowisata = mongoose.model('Agrowisata', agrowisataSchema);
